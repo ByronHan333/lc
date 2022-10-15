d341dca81cb15d1f40e23b86a954b3fa34af8d0e
ByronHan333 <zhan12@dons.usfca.edu>
2022-10-15 16:01:23 -0700

    git

--- ruby/1313.rb
+++ ruby/1313.rb
@@ -1,7 +1,17 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def decompress_rl_elist(nums)
-
+  res = []
+  len_paris = nums.length / 2
+  puts len_paris
+  (0...len_paris).each do |idx|
+    freq = nums[idx * 2]
+    val = num[idx * 2 + 1]
+    res += val.to_s * freq
+    puts val.to_s * freq
+  end
+  res
 end
 
-
+nums = [1, 1, 2, 3]
+decompress_rl_elist(nums)
