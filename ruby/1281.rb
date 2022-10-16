c56074ecbf968fd0970efb0152b1def169369676
ByronHan333 <zhan12@dons.usfca.edu>
2022-10-16 13:00:03 -0700

    add

--- ruby/1281.rb
+++ ruby/1281.rb
@@ -1,5 +1,8 @@
 # @param {Integer} n
 # @return {Integer}
 def subtract_product_and_sum(n)
-
+  digits = n.to_s.split('').map(&:to_i)
+  product = digits.reduce{|acc,el| acc * el}
+  sum = digits.reduce{|acc,el| acc + el}
+  product - sum
 end
