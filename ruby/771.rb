0818cbfe70d00f5150ea75a04caa6ffc105ead38
ByronHan333 <zhan12@dons.usfca.edu>
2022-10-16 12:21:46 -0700

    add

--- ruby/771.rb
+++ ruby/771.rb
@@ -1,7 +1,16 @@
 # @param {String} jewels
 # @param {String} stones
 # @return {Integer}
-def num_jewels_in_stones(jewels, stones); end
+def num_jewels_in_stones(jewels, stones)
+  stones.count(jewels)
+end
 
-jewels = 'z', stones = 'ZZ'
-jewels = 'aA', stones = 'aAAbbbb'
+# jewels = 'z'
+# stones = 'ZZ'
+jewels = 'aA'
+stones = 'aAAbbbb'
+
+p num_jewels_in_stones(jewels,stones)
+
+p jewels.is_a?(String)
+p stones.is_a?(String)
