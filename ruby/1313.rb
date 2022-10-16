b3f00ec3812a21f0a870157206abe310bd1fee5c
ByronHan333 <zhan12@dons.usfca.edu>
2022-10-16 11:55:14 -0700

    add

--- ruby/1313.rb
+++ ruby/1313.rb
@@ -3,15 +3,14 @@
 def decompress_rl_elist(nums)
   res = []
   len_paris = nums.length / 2
-  puts len_paris
   (0...len_paris).each do |idx|
     freq = nums[idx * 2]
-    val = num[idx * 2 + 1]
-    res += val.to_s * freq
-    puts val.to_s * freq
+    val = nums[idx * 2 + 1]
+    res += [val] * freq
   end
   res
 end
 
 nums = [1, 1, 2, 3]
-decompress_rl_elist(nums)
+# nums = [1,2,3,4]
+p decompress_rl_elist(nums)
