1c833c48ca264e6e17c88a8468833232589a8817
ByronHan333 <zhan12@dons.usfca.edu>
2022-10-16 12:00:51 -0700

    test

---
+++ ruby/771.rb
@@ -0,0 +1,9 @@
+# @param {String} jewels
+# @param {String} stones
+# @return {Integer}
+def num_jewels_in_stones(jewels, stones)
+
+end
+
+jewels = "z", stones = "ZZ"
+jewels = "aA", stones = "aAAbbbb"
